37cf951bddb5558d058511e6efd465fea664856e
Victor Winberg <dat14vwi@student.lu.se>
2015-11-29 14:45:37 +0100

    Dsek update

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 angular.module('website', ['ngRoute']).
   config(function($routeProvider, $locationProvider) {
     $routeProvider.
-      when('/about', {
+      when('/nyheter', {
         templateUrl:'partials/basic-template.html',
-        controller:'AboutController'
+        controller:'NewsController'
       }).
       when('/experiments', {
         templateUrl:'partials/basic-template.html',
@@ -14,28 +14,47 @@ angular.module('website', ['ngRoute']).
         controller:'PersonCtrl'
       }).
       otherwise({
-        redirectTo:'/home',
+        redirectTo:'/',
         templateUrl:'partials/basic-template.html',
         controller:'HomeController'
       });
-      // use the HTML5 History API
-      $locationProvider.html5Mode({
-        enabled: true,
-        requireBase: false
-      });
+    // use the HTML5 History API
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+    });
+  }).
+  controller('HeaderController', function($scope) {
+    $scope.logopath = 'img/dseklogo.svg';
+    $scope.dropdowns =
+    [
+      {title: 'Aktuellt', items: [
+        {title: 'Nyheter', href: '/nyheter'},
+        {title: 'Kalender', href: '/kalender'},
+        {title: 'Eventanmälan', href: '/eventanmälan'}]},
+      {title: 'Community', items: [
+        {title: 'Anslut', href: '/anslut'}]},
+      {title: 'För funktionärer', items: [
+        {title: 'STAB16', href: '/stab16'}]},
+      {title: 'Sektionen', items: [
+        {title: 'Om sektionen', href: '/sektionen'},
+        '-',
+        {title: 'Swagtest', href: '/swagtest'}]},
+      {title: 'För företag', href: '/foretag'}
+    ];
   }).
   controller('HomeController', function ($scope) {
     $scope.title = 'Welcome to angularjs';
     $scope.body = 'This is the projects homepage';
   }).
-  controller('AboutController', function($scope) {
-    $scope.title = 'About page';
-    $scope.body = 'This is the about page';
+  controller('TopicalController', function($scope) {
+    $scope.title = 'Aktuellt';
+    $scope.body = 'Dsek.se renoveras om';
   }).
   controller('ExperimentsController', function($scope) {
     $scope.title = 'Experiments';
     $scope.body = 'Warning this is experimental';
   }).
   controller('PersonCtrl', function($scope) {
-    
+
   });
