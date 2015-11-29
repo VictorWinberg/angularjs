5be8c8e23a1524b53b40d68b0695bb049b0a49a8
Victor Winberg <dat14vwi@student.lu.se>
2015-11-29 11:16:46 +0100

    Angularjs update and local nodeserver added

--- js/app.js
+++ js/app.js
@@ -1,11 +1,41 @@
 angular.module('website', ['ngRoute']).
-  config(function($routeProvider) {
+  config(function($routeProvider, $locationProvider) {
     $routeProvider.
-      when('/about', {templateUrl:'partials/about.html'}).
-      when('/experiments', {templateUrl:'partials/experiments.html'}).
-      otherwise({redirectTo:'/home', templateUrl:'partials/home.html'})
+      when('/about', {
+        templateUrl:'partials/basic-template.html',
+        controller:'AboutController'
+      }).
+      when('/experiments', {
+        templateUrl:'partials/basic-template.html',
+        controller:'ExperimentsController'
+      }).
+      when('/personakt', {
+        templateUrl:'partials/personakt.html',
+        controller:'PersonCtrl'
+      }).
+      otherwise({
+        redirectTo:'/home',
+        templateUrl:'partials/basic-template.html',
+        controller:'HomeController'
+      });
+      // use the HTML5 History API
+      $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+      });
   }).
-  controller('MainController', function ($scope) {
-      $scope.title = 'Welcome to angularjs';
-      $scope.body = 'This is a project by Victor Winberg';
+  controller('HomeController', function ($scope) {
+    $scope.title = 'Welcome to angularjs';
+    $scope.body = 'This is the projects homepage';
+  }).
+  controller('AboutController', function($scope) {
+    $scope.title = 'About page';
+    $scope.body = 'This is the about page';
+  }).
+  controller('ExperimentsController', function($scope) {
+    $scope.title = 'Experiments';
+    $scope.body = 'Warning this is experimental';
+  }).
+  controller('PersonCtrl', function($scope) {
+    
   });
